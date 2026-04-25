414865e5e01de4a9ceb26dd018cc2dadc894733e
nikhil-sharma00 <ns721764@gmail.com>
2026-04-25 10:18:59 +0530

    completed AI service , settuped routes for AI query via express

--- server/routes/aiRouter.js
+++ server/routes/aiRouter.js
@@ -1,41 +1,50 @@
 import { Router } from "express";
 import { handleEmbedding, handleQuery } from "../services/aiService.js";
+import axios from 'axios'
 
 export const aiRouter = Router();
 
-// aiRouter.post("/query", async (req, res) => {
-//   const { question } = req.body;
-//   if (!question?.trim()) {
-//     return res.status(400).json({ error: "Question is required" });
-//   }
+PYTHON_AI_URI = process.env.PYTHON_AI_URI || 'http://localhost:8000'
 
-//   try {
-//     const result = await handleQuery(question, req.user.id);
-//     res.json(result);
-//   } catch (err) {
-//     console.error("AI query error:", err);
-//     res.status(500).json({
-//       error: err?.message || "AI query failed",
-//     });
-//   }
-// });
+
+aiRouter.post("/query" , async(req,res)=>{
+  const {question} = req.body
+
+  if(!question.trim()){
+    return res.status(400).json({error : "Question is required"})
+  }
+
+  try{
+    const {data} = await axios.post(`${PYTHON_AI_URI}/query`,{
+      question,
+      user_id = req.user.id
+    })
+    res.json(data)
+  }catch(error){
+    console.error('Python call failed! : ',error.response?.data || err.message)
+    res.status(500).json({
+      error : err.response?.data?.detail || "Ai service unavailable"
+    })
+  }
+})
+
 
 aiRouter.post("/embed", async (req, res) => {
   const { docId, title, content } = req.body;
   if (!docId) return res.status(400).json({ error: "docId required" });
 
-  try {
-    await handleEmbedding({
-      docId,
-      title: title ?? "",
-      content: content ?? "",
-      userId: req.user.id,
-    });
-    res.json({ ok: true });
-  } catch (err) {
-    console.error("Embed error:", err);
-    res.status(500).json({
-      error: err?.message || "Embedding failed",
-    });
+  try{
+    await axios.post(`${PYTHON_AI_URI}/embed`,{
+      doc_id = docId,
+      title,
+      content
+      user_id = req.user.id
+    })
+
+    res.json({ok :true})
+  }catch(err){
+    console.error('Python embed call failed : ',err.response?.data || err.message)
+    res.status(500).json({error: err.response?.data?.detail || 'Embedding service unavailable'})
   }
 });
+
