3be0fb3a3617e85cf0f317c5da882a235f1b7039
nikhil-sharma00 <ns721764@gmail.com>
2026-04-30 08:10:08 +0530

    cors issue

--- server/server.js
+++ server/server.js
@@ -10,20 +10,24 @@ dotenv.config()
 const app  = express()
 const PORT = process.env.PORT || 5000
 
-const allowedOrigins = [
-    'https://dev-docs-w7mp.vercel.app/',
-    process.env.FRONTEND_URL
-].filter(Boolean)
+// const allowedOrigins = [
+//     'https://dev-docs-w7mp.vercel.app/',
+//     process.env.FRONTEND_URL,
+// ].filter(Boolean)
+
+// app.use(cors({
+//     origin : (origin,callback)=>{
+//         if(!origin || allowedOrigins.includes(origin)) return callback(null,true);
+//         callback(new Error("Not allowed by CORS"));
+//     },
+//     credentials:true
+// }))
 
 app.use(cors({
-    origin : (origin,callback)=>{
-        if(!origin || allowedOrigins.includes(origin)) return callback(null,true);
-        callback(new Error("Not allowed by CORS"));
-    },
-    credentials:true
-}))
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
+}));
 
-// app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }))
 app.use(express.json())
 
 
